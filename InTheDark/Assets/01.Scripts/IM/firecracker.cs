b36640097c76ec425b19e45ff15052a154f459df
Vmfptlffk <104883488+Vmfptlffk@users.noreply.github.com>
2023-07-20 17:57:44 +0900

    아이템 프리팹 추가

--- InTheDark/Assets/01.Scripts/IM/firecracker.cs
+++ InTheDark/Assets/01.Scripts/IM/firecracker.cs
@@ -4,28 +4,61 @@ using UnityEngine;
 
 public class firecracker : MonoBehaviour
 {
-    public GameObject fire;
+    public GameObject fire; 
     public ParticleSystem particle;
-    
+
+    public Transform cam;
+    public Transform player;
+
+    public float throwForce = 5f; //던지는 힘
+    public float throwTorque = 10f;// 던지는 높이
+
+    public int firecount = 8;
+
+    public float throwdown = 0.1f; //던져진 힘 줄임
+
+    bool throwready; //던질 수 있는지 확인
+    Rigidbody rb;
 
 
     private void Start()
     {
-     particle = GetComponentInChildren<ParticleSystem>();
+        particle = GetComponentInChildren<ParticleSystem>();
+        throwready = true;
+        rb = GetComponent<Rigidbody>();
         
     }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E))
+        if(Input.GetKeyDown(KeyCode.E)&&throwready)
         {
             if (!particle.isPlaying)
             {
                 particle.Play();
+                Throw();
             }
             
         }
     }
+    void Throw()
+    {
+        throwready = false;
 
+        //던질 오브젝트 생성
+        GameObject firecracker = Instantiate(fire,player.position,cam.rotation);
+
+        //보고있는 방향으로 던지기
+        Vector3 force = cam.transform.forward * throwForce + transform.up * throwTorque;
+        rb.AddForce(force,ForceMode.Impulse);
+
+        Invoke(nameof(ResetThrow), throwdown);
+
+        firecount--;
+    }
+    void ResetThrow()
+    {
+        throwready = true;
+    }
 
 
    
