6a4090e636c18755345ccc3e26a13f85293f8fed
Vmfptlffk <104883488+Vmfptlffk@users.noreply.github.com>
2023-07-24 17:49:06 +0900

    부적 스크립트 수정, 아이템 스폰 추가, 아이템 프리팹 수정

---
+++ InTheDark/Assets/01.Scripts/IM/Item/amulet.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class amulet : MonoBehaviour
+{
+    public GameObject amulets;
+    public GameObject enemy;
+    MoveAgent moveAgent;
+    public LayerMask laymask;// 레이어 저장
+    
+
+    bool stun;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartCoroutine(Stun());
+        }
+    }
+
+    IEnumerator Stun()
+    {
+        
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit rayhit;
+
+        if(Physics.Raycast(ray, out rayhit,10,laymask))
+        {
+            if (rayhit.collider.CompareTag("ENEMY"))
+            {
+                moveAgent.Stop();
+            }
+            
+        }
+      
+        yield return new WaitForSecondsRealtime(3f);
+
+    }
+
+
+}
